593fc3f55b21ee8d4f9c1b4fc559e764ca9e4842
72xarth <pikachu0940188505@gmail.com>
2024-03-09 20:44:54 +0700

    Update star.ts

--- api/star.ts
+++ api/star.ts
@@ -1,41 +1,50 @@
 import express from "express";
-import { conn } from "../dbconnect";
-import {  TripPostStarRespone } from "../model";
 import mysql from "mysql";
+import { conn } from "../dbconnect";
 
 export const router = express.Router();
 
-router.get("/", (req, res) => {
-    conn.query('select * from Stars_S', (err, result, fields)=>{
-      res.json(result);
-    });
-  });
+router.get("/:name", (req, res) => {
+    let responseData = {
+      persons: [],
+      movies: []
+    };
   
+    let sql = `SELECT Person.* FROM Person WHERE pid IN (
+      SELECT Stars_S.SPID FROM Stars_S
+      INNER JOIN Movie ON Stars_S.SMID = Movie.mid
+      WHERE Movie.name LIKE ?
   
-  router.post("/", (req, res) => {
-    let star: TripPostStarRespone = req.body;
-    let sql =
-      "INSERT INTO `Stars_S`(`SMID`, `SPID`) VALUES (?,?)";
-    sql = mysql.format(sql, [
-     star.SMID,
-     star.SPID
-     
-    ]);
+      UNION
+  
+      SELECT Creators_C.CPID FROM Creators_C
+      INNER JOIN Movie ON Creators_C.CMID = Movie.mid
+      WHERE Movie.name LIKE ?
+  )`;
+    sql = mysql.format(sql, [`%${req.params.name}%`, `%${req.params.name}%`]); // Use the same parameter twice
     conn.query(sql, (err, result) => {
-      if (err) throw err;
-      res
-        .status(201)
-        .json({ affected_row: result.affectedRows, last_idx: result.insertId });
+      if (err) {
+        res.status(400).json(err);
+      } else {
+        responseData.persons = result;
+        checkAndSendResponse();
+      }
     });
-  });
   
-  router.delete("/", (req, res) => {
-    conn.query(
-        "DELETE FROM Stars_S WHERE SMID = ? AND SPID = ?",
-        [req.query.mid, req.query.pid],
-        (err, result, fields) => {
-            if (err) throw err;
-            res.json(result);
-        }
-    );
-});
+    let sql1 = 'SELECT * FROM Movie WHERE name LIKE ?';
+    sql1 = mysql.format(sql1, [`%${req.params.name}%`]);
+    conn.query(sql1, (err, result) => {
+      if (err) {
+        res.status(400).json(err);
+      } else {
+        responseData.movies = result;
+        checkAndSendResponse();
+      }
+    });
+  
+    function checkAndSendResponse() {
+      if (responseData.persons.length > 0 && responseData.movies.length > 0) {
+        res.json(responseData);
+      }
+    }
+  });
