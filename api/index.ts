0e726631729996615803ca879034d3173348025c
72xarth <pikachu0940188505@gmail.com>
2024-03-09 20:55:58 +0700

    2

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-export const router = express.Router();
-
-router.get('/', (req, res)=>{
-    let body = req.body
-    res.send('Get in index.ts body:' + body);
-});
